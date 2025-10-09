a6b1faa37634837ef9993fc5697144670301d02b
C-UGENYICOS101 <chigozirimugenyi@gmail.com>
2025-10-09 01:00:33 +0100

    Add files

---
+++ week_2/practice_1.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Welcome to CSC 101!");
+    println!("The course learning outcome is as follows:");
+    println!("1. Distinguish between computer and computer programming,");
+    println!("2. Develop some techniques in computer science,");
+    println!("3. Understand the different areas of study in computer science,");
+    println!("4. Be conversant with applications of computer science,");
+    println!("5. Navigate the career prospects in computer science,");
+    println!("6. Be conversant with computer programming concepts");
+}
