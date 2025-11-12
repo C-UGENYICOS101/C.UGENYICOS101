839d49ee2542c8f529bc7ad0c929a00ad90bb8a4
C-UGENYICOS101 <chigozirimugenyi@gmail.com>
2025-11-12 17:20:08 +0100

    Function Invocation

---
+++ week-7/Project/target/debug/Project.d
@@ -0,0 +1,1 @@
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\week-7\Project\target\debug\Project.exe: C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\week-7\Project\src\main.rs
---
+++ week-7/Project/target/debug/deps/Project.d
@@ -0,0 +1,5 @@
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\week-7\Project\target\debug\deps\Project.d: src\main.rs
+
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\week-7\Project\target\debug\deps\Project.exe: src\main.rs
+
+src\main.rs:
