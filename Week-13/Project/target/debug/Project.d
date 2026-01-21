fddfd0b77b61df656a470fb2e8f3f2f4e0122228
C-UGENYICOS101 <chigozirimugenyi@gmail.com>
2026-01-21 21:32:25 +0100

    Rust Database Schema Access Manager

---
+++ Week-13/Project/target/debug/Project.d
@@ -0,0 +1,1 @@
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\Week-13\Project\target\debug\Project.exe: C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\Week-13\Project\src\main.rs
---
+++ Week-13/Project/target/debug/deps/Project.d
@@ -0,0 +1,5 @@
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\Week-13\Project\target\debug\deps\Project.d: src\main.rs
+
+C:\Users\chigo\OneDrive\Documents\C.UGENYICOS101\Week-13\Project\target\debug\deps\Project.exe: src\main.rs
+
+src\main.rs:
